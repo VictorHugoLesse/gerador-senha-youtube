23089a1c9d6dd87896a94617c359f67133666c9c
Victor Hugo <113568220+VictorHugoLesse@users.noreply.github.com>
2023-10-24 01:27:40 -0300

    Finish

--- script.js
+++ script.js
@@ -4,32 +4,32 @@ let buttonElement = document.querySelector("#button");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
-let containerPassword = document.querySelector("#container-password")
+let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$@!0123456789"
-let novaSenha = "";
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function() {
   sizePassword.innerHTML = this.value;
 }
 
-function generatePassword(){
-  
 
-  let pass = "";
+function generatePassword(){
+
+  let pass = '';
   for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-  pass += charset;charAt(Math.floor(Math.random() * n)) 
+    pass += charset.charAt(Math.floor(Math.random() * n));
   }
- 
+  
+  console.log(pass)
   containerPassword.classList.remove("hide");
   password.innerHTML = pass;
+  novaSenha = pass;
 }
 
 function copyPassword(){
-  alert("Senha copiada com sucesso")
+  alert("Senha copiada com sucesso!")
   navigator.clipboard.writeText(novaSenha);
 }
-
-
